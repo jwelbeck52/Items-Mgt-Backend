de89f6e145e3786005ec3731c73b230cd52572fd
jwelbeck52 <jwelbeck38@gmail.com>
2021-05-17 12:17:58 +0000

    feat: add api controller, model, migration for items, categories; add api route for item, category, wishlist

---
+++ database/migrations/2021_05_17_105102_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('price');
+            $table->boolean('isOwned');
+            $table->string('status');
+            $table->string('src_name');
+            $table->string('src_link')->nullable();
+            $table->string('img_src');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
