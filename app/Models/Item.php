4b6d65f33fa0c73a0ac727597586c11d4e731899
jwelbeck52 <jwelbeck38@gmail.com>
2021-05-17 12:42:33 +0000

    feat: add fillables in item and category model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'desccription',
+        'price',
+        'status',
+        'src_name',
+        'src_link',
+        'img_src'
+    ];
+
 }
